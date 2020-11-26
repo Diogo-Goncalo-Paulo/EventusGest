589c757eb3aa865b7b9517401395770346f305f5
gust500 <madaritoto@gmail.com>
2020-11-26 18:18:39 +0000

    update Carrier table

--- backend/views/carrier/index.php
+++ backend/views/carrier/index.php
@@ -12,33 +12,75 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="carrier-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Carrier', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
-            'info',
-            'photo',
-            'idCredential',
-            //'idCarrierType',
-            //'createdAt',
-            //'updatedAt',
-            //'deletedAt',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
 
 
+    <div class="card bg-transparent border-0 mb-3">
+        <div class="card-header bg-transparent border-0 p-0">
+            <h1 class="d-inline"><?= Html::encode($this->title) ?></h1>
+            <div class="float-right mt-1">
+                <a class="btn btn-default radius-round" data-toggle="collapse" href="#collapseSearch" role="button" aria-expanded="false" aria-controls="collapseSearch">
+                    <i class="fas fa-search"></i>
+                </a>
+
+                <?php
+                echo Html::a('<i class="fas fa-plus"></i>', ['create'], ['data-toggle' => 'tooltip', 'class' => 'btn btn-outline-success radius-round', 'id' => 'btnCreate', 'title' => 'Novo Carregador']);
+                //} ?>
+            </div>
+        </div>
+        <div class="collapse" id="collapseSearch">
+            <div class="card-body">
+                <?= $this->render('_search', ['model' => $searchModel]) ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="card bg-white p-3">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'layout' => '{items}<div class="d-flex"><span class="mr-auto">{summary}</span>{pager}</div>',
+            'summary' => 'A mostrar <b>{begin}-{end}</b> de <b>{totalCount}</b>.',
+
+            'tableOptions' => [
+                'class'=>'table table-eg table-hover'
+            ],
+            'columns' => [
+                [
+                    'label' => 'Nome',
+                    'value' => 'name'
+                ],
+                [
+                    'label' => 'Info',
+                    'value' => 'info'
+                ],
+                [
+                    'label' => 'Foto',
+                    'value' => 'photo'
+                ],
+                [
+                    'label' => 'Credencial',
+                    'value' => 'idCredential0.name'
+                ],
+                [
+                    'label' => 'Tipo de Carregador',
+                    'value' => 'idEntityType0.name'
+                ],
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{view} {update} {delete}',
+                    'buttons' => [
+                        'view' => function ($url, $model, $key) {
+                            return Html::a('<i class="fas fa-eye"></i>', ['view', 'id' => $model->id], ['data-toggle' => 'tooltip', 'title' => 'Ver', 'class' => 'btn btn-sm btn-action btn-primary']);
+                        },
+                        'update' => function ($url, $model, $key) {
+                            return Html::a('<i class="fa fa-pencil"></i>', ['update', 'id' => $model->id],['data-toggle' => 'tooltip', 'title' => 'Editar', 'class' => 'btn btn-sm btn-action btn-success']);
+                        },
+                        'delete' => function ($url, $model, $key) {
+                            return Html::a('<i class="fas fa-trash-alt"</i>', ['delete', 'id' => $model->id], ['data-toggle' => 'tooltip', 'title' => 'Apagar', 'class' => 'btn btn-sm btn-action btn-danger', 'data-method' => 'post']);
+
+                        },
+                    ]
+                ]
+            ]
+        ]); ?>
+    </div>
 </div>
