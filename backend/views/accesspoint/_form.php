d1a2f4c382381125d44ee108e886ac45141492c9
paulom12 <2190746@my.ipleiria.pt>
2020-11-26 17:20:26 +0000

    EG-156

--- backend/views/accesspoint/_form.php
+++ backend/views/accesspoint/_form.php
@@ -8,6 +8,17 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Accesspoint */
 /* @var $form yii\widgets\ActiveForm */
+/*
+$js = <<<SCRIPT
+$(document).ready(function() { 
+    $('#area-1').change(function(){
+        var selectedArea = $(this).children('option:selected').val();
+        
+        $('#area-1 option[value= selectedArea]').remove();
+    });
+}); 
+SCRIPT;
+$this->registerJs($js);*/
 ?>
 
 <div class="accesspoint-form">
@@ -22,7 +33,7 @@ use yii\widgets\ActiveForm;
                 <label class="control-label" for="area-1">Área 1</label>
                 <?php echo Select2::widget([
                     'name' => 'Accesspoint[area1]',
-                    'items' => ArrayHelper::map(\app\models\Area::find()->all(), 'id', 'nome'),
+                    'items' => ArrayHelper::map(\app\models\Area::find()->where(['idEvent' => Yii::$app->user->identity->getEvent()])->all(), 'id', 'nome'),
                     'options' => ['class' => 'w-100', 'id' => 'area-1']
                 ]); ?>
                 <div class="help-block"></div>
@@ -34,8 +45,8 @@ use yii\widgets\ActiveForm;
                 <label class="control-label" for="area-2">Área 2</label>
                 <?php echo Select2::widget([
                     'name' => 'Accesspoint[area2]',
-                    'items' => ArrayHelper::map(\app\models\Area::find()->all(), 'id', 'nome'),
-                    'options' => ['class' => 'w-100', 'id' => 'area-2']
+                    'items' => ArrayHelper::map(\app\models\Area::find()->where(['idEvent' => Yii::$app->user->identity->getEvent()])->all(), 'id', 'nome'),
+                    'options' => ['class' => 'w-100', 'id' => 'area-2'],
                 ]); ?>
                 <div class="help-block"></div>
             </div>
@@ -46,6 +57,5 @@ use yii\widgets\ActiveForm;
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
-
+    <?php ActiveForm::end();?>
 </div>
