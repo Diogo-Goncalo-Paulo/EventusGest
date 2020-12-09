35706ecec4b95bd69bc85aab48f079353ad2c79e
gust500 <madaritoto@gmail.com>
2020-12-09 16:46:28 +0000

    Relation Entitytype  -> Areas

--- backend/views/entitytype/_form.php
+++ backend/views/entitytype/_form.php
@@ -1,5 +1,7 @@
 <?php
 
+use pcrt\widgets\select2\Select2;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -15,6 +17,16 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'qtCredentials')->textInput() ?>
+
+    <div class="form-group field-user-role">
+        <label class="control-label" for="user-role">Areas</label>
+        <?php echo Select2::widget([
+            'name' => 'Entitytype[areas]',
+            'items' => ArrayHelper::map(\app\models\Area::find()->where(['deletedAt' => null])->andWhere(['idEvent' => Yii::$app->user->identity->getEvent()])->all(), 'id', 'name'),
+            'options' => ['class' => 'w-100', 'id' => 'entitytype-areas','multiple' => true]
+        ]); ?>
+        <div class="help-block"></div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
