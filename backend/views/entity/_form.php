5eb1dcc039fbccaabfb43258a60cad2197553ea8
paulom12 <paulo.macas4@gmail.com>
2022-08-30 21:14:40 +0100

    EG-342

--- backend/views/entity/_form.php
+++ backend/views/entity/_form.php
@@ -23,12 +23,17 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <?php
-    echo $form->field($model, 'idEntityType')->widget(Select2::className(), ['options' => ['placeholder' => 'Selecione'], 'items'=>ArrayHelper::map($entityType,'id','name')]); ?>
+    echo $form->field($model, 'idEntityType')->widget(Select2::className(), ['options' => ['placeholder' => 'Selecione', 'id' => 'entityType'], 'items' => ArrayHelper::map($entityType,'id','name')]); ?>
+
+    <div class="form-group w-25">
+        <label class="control-label" for="q">Quantidade de credenciais a criar</label>
+        <?= Html::textInput('q', 0, ['type' => 'number', 'class' => 'form-control', 'id' => 'q']) ?>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
-
 </div>
+
